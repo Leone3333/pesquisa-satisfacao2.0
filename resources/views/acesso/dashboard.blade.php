d0a0d51601a62b26b7aef6021fe6d40cf45a50b5
Leone <leone.almeida@ba.estudante.senai.br>
2024-07-23 22:04:25 -0300

    dashboard-update1.0

--- resources/views/acesso/dashboard.blade.php
+++ resources/views/acesso/dashboard.blade.php
@@ -1,89 +1,101 @@
-@extends('layouts.mainLogin')
+@extends('layouts.mainDashboard')
 
 @section('title', 'Dashbord')
 
 <body>
-    <h1>Dashbord</h1>
-    {{--
-    @dump($user);
-    @dump($graficoPizza); --}}
+    <!-- As a link -->
+    <nav class="navBar">
+        <h1>Dashboard</h1>
+        <div class="navButtons">
+            <form action="/" method="get">
+                <button class="navButton" type="submit">Home</button>
+            </form>
 
-    <form action="/dashboard" method="post">
-        @csrf
-        <label for="formulario"> <br>Feedbacks<br></label>
-        <select id="formsTipoFeedback" name="tipoFeedback">
-            <option value="1">Muito satisfeito</option>
-            <option value="2">Insatisfeito</option>
-            <option value="3">Muito insatisfeito</option>
-        </select>
-        <input type="submit">
-    </form>
-
-    <form action="/dashboard" method="post">
-        @csrf
-        <label for="formulario"> <br>Setores<br></label>
-        <select id="formsSetor" name="setorFeedback">
-            <option value="atendimento">atendimento</option>
-            <option value="organização">organização</option>
-            <option value="limpeza">limpeza</option>
-            <option value="caixa">caixa</option>
-            <option value="outros">outros</option>
-        </select>
-        <input type="submit">
-    </form>
-
-
-    <form action="/" method="get">
-        <button type="submit">Home</button>
-    </form>
-
-    <form action="/dashboard" method="get">
-        <button type="submit">Sair</button>
-    </form>
-
-    @if (isset($user))
-        <table>
-            <th>Email</th>
-            <th>-----------</th>
-            <th>Senha</th>
-            <tr>
-                <td>{{ $user['user']['email'] }}</td>
-                <td>-----------</td>
-                <td>{{ $user['user']['senha'] }}</td>
-            </tr>
-        </table>
-    @endif
-
-    @if (isset($table))
-        <div class="table">
-
-            @foreach ($table as $line)
-                <h4 style="color: #000"> Tipo de feedback:
-                    {{ $line['tipoFeedback'] === '2'
-                        ? 'Insatisfeito'
-                        : ($line['tipoFeedback'] === '1'
-                            ? 'Satisfeito'
-                            : ($line['tipoFeedback'] === '3'
-                                ? 'Muito insatisfeito'
-                                : $line['tipoFeedback'])) }}
-                </h4>
-                <h4 style="color: #000"> Setor: {{ $line['setor'] }}</h4>
-                <h4 style="color: #000"> Comentario: {{ $line['comentario'] }}</h4>
-                <hr style="color:bisque">
-            @endforeach
+            <form action="/dashboard" method="get">
+                <button class="navButtonReload" type="submit">Recarregar</button>
+            </form>
         </div>
-    @endif
+    </nav>
 
-    <div style="width: 500px" class="chartPie">
-        <h2>Grafico de pizza</h2>
-        {!! $graficoPizza->container() !!}
+
+    {{-- @dump($graficoPizza->container()); --}}
+
+    <div class="container-charts">
+        <div class="div-table">
+            <h1>Comentarios</h1>
+            <table class="table">
+                <div class="filters">
+                    <form action="/dashboard" method="post">
+                        @csrf
+                        <div class="selected-feedback">
+                    <div class="selected-feedback-labels">
+                        <label>Feedbacks</label>
+                        <select class="select-class" id="formsTipoFeedback" name="tipoFeedback">
+                            <option value="1">Muito satisfeito</option>
+                            <option value="2">Insatisfeito</option>
+                            <option value="3">Muito insatisfeito</option>
+                        </select>
+                    </div>
+                        <input class="selectedButton" type="submit">
+                    </div>
+                    </form>
+
+                    <form action="/dashboard" method="post">
+                        @csrf
+                        <div class="selected-feedback">
+                            <div class="selected-feedback-labels">
+                        <label for="formulario">Setores</label>
+                        <select id="formsSetor" name="setorFeedback">
+                            <option value="atendimento">atendimento</option>
+                            <option value="organização">organização</option>
+                            <option value="limpeza">limpeza</option>
+                            <option value="caixa">caixa</option>
+                            <option value="outros">outros</option>
+                        </select>
+                            </div>
+                        <input class="selectedButton" type="submit">
+                    </div>
+                    </form>
+                </div>
+
+                <tr>
+                    <th>Tipo Feedback</th>
+                    <th>Setor</th>
+                    <th>Comentario</th>
+                </tr>
+
+                @if (isset($table))
+                    @foreach ($table as $line)
+                        <tr>
+                            <td>
+                                {{ $line['tipoFeedback'] === '2'
+                                    ? 'Insatisfeito'
+                                    : ($line['tipoFeedback'] === '1'
+                                        ? 'Satisfeito'
+                                        : ($line['tipoFeedback'] === '3'
+                                            ? 'Muito insatisfeito'
+                                            : $line['tipoFeedback'])) }}
+                            </td>
+
+                            <td>{{ $line['setor'] }}</td>
+
+                            <td>{{ $line['comentario'] }}</td>
+                    @endforeach
+                @endif
+                </tr>
+            </table>
+        </div>
+
+        <div style="width: 500px" class="chartPie">
+            <h2>Grafico de pizza</h2>
+            {!! $graficoPizza->container() !!}
+        </div>
+
+        <div style="width: 500px" class="chartPie">
+            <h2>Grafico de barra</h2>
+            {!! $graficoBarra->container() !!}
+        </div>
     </div>
-
-    <div style="width: 500px" class="chartPie">
-        <h2>Grafico de barra</h2>
-        {!! $graficoBarra->container() !!}
-    </div>
-
 
 
 
